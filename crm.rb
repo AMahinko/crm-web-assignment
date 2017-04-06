6cc8838e64657b188f7a737a6c3f4a85f90c99fa
Alex <alex.mahinko@gmail.com>
2017-04-06 18:13:52 -0400

    Header and nav implemented. CRM now has a ticker of how many contacts exist in total

--- crm.rb
+++ crm.rb
@@ -1,10 +1,10 @@
 require_relative "contact.rb"
 require "sinatra"
 index_counter = 0
-
 ###Test Data
 
-Contact.create("Randall", "Trager", "Tragermeister@nss_exodus.nt", "Worrying obsession with hair dye. Possibly robosexual.")
+Contact.create("Karl", "Hungus", "k_hung@treehornpictures.com", "He come to fix the cable.")
+Contact.create("Randall", "Trager", "tragermeister@nss_exodus.nt", "Worrying obsession with hair dye. Possibly robosexual.")
 Contact.create("Leon", "Voronin", "major_v@schreknet.vtm", "Russian bootleg James Bond. Despises cheetos. Silence affordable.")
 Contact.create("Richter", "Monroe", "rmonroe@swarmvpn.net", "Will hack 4 opiates.")
 
@@ -21,11 +21,11 @@ end
 
 get "/contacts" do
 
-
+@contacts_ticker = "There are #{Contact.all.length} contacts"
 erb :contacts
 end
 
-get "/contacts/new" do
+get "/new" do
 
 
   erb :contact_new
